7a91f9073fec25503a7ace1003b5606019b491b2
Marian Boda <marianboda@gmail.com>
2020-02-08 16:09:11 +0100

    Added some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './App.scss';
 
 export default () => (
   <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to new React Parcel Micro App!</h1>
+    <p className="main-message">Hard to get more minimal than this React app.</p>
   </>
 );
